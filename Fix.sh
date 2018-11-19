58695a107b10746da1e535017031c91ae863a204
DzNk <45180299+DzNk@users.noreply.github.com>
2018-11-19 23:13:20 +0400

    Update Fix.sh

--- Fix.sh
+++ Fix.sh
@@ -1,5 +1,5 @@
 wget https://raw.githubusercontent.com/DzNk/FixNethunter/master/sources.list
-sudo mv ./source.list /etc/apt/sources.list
+sudo mv ./sources.list /etc/apt/sources.list
 gpg --keyserver hkp://keys.gnupg.net --recv-key 7D8D0BF6
 gpg -a --export 7D8D0BF6 | apt-key add -
 apt update
