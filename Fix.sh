1b5f537fbb222b9a672de08ec9f1c6c3556b2dba
DzNk <45180299+DzNk@users.noreply.github.com>
2018-11-19 22:59:53 +0400

    Delete Fix.sh

--- Fix.sh
+++
@@ -1,7 +0,0 @@
-wget https://raw.githubusercontent.com/DzNk/FixNethunter/master/sources.list
-rm /etc/apt/sources.list
-mv sources.list /etc/apt/
-gpg --keyserver hkp://keys.gnupg.net --recv-key 7D8D0BF6
-gpg -a --export 7D8D0BF6 | apt-key add -
-apt update
- 
